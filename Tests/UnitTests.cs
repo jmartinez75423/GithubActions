b83bcec9b0f6c3537a54b4ec340823be782d8f1d
jmartinez75423 <142929714+jmartinez75423@users.noreply.github.com>
2025-03-28 17:39:58 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Martinez()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Martinez()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,7 +20,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Martinez()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -28,7 +28,7 @@ public class Addition
 	}
 
  	[TestMethod]
-    public void Subtract_Valid()
+    public void Subtract_Valid_Martinez()
     {
         Assert.AreEqual(3, Program.Subtract("10", "5"));
         Assert.AreEqual(5, Program.Subtract("10", "5"));
@@ -37,7 +37,7 @@ public class Addition
     }
 
     	[TestMethod]
-    public void Multiply_Valid()
+    public void Multiply_Valid_Martinez()
     {
         Assert.AreEqual(6, Program.Multiply("2", "3"));
         Assert.AreEqual(0, Program.Multiply("0", "5"));
@@ -45,7 +45,7 @@ public class Addition
     }
 
     	[TestMethod]
-    public void Divide_Valid()
+    public void Divide_Valid_Martinez()
     {
         Assert.AreEqual(5, Program.Divide("10", "2"));
         Assert.AreEqual(1, Program.Divide("3", "3"));
@@ -53,7 +53,7 @@ public class Addition
     }
 
         [TestMethod]
-    public void Power_Valid()
+    public void Power_Valid_Martinez()
     {
         Assert.AreEqual(8, Program.Power("2", "3"));
         Assert.AreEqual(1, Program.Power("5", "0"));
@@ -61,13 +61,13 @@ public class Addition
     }
 
     	[TestMethod]
-    public void Divide_ByZero()
+    public void Divide_ByZero_Martinez()
     {
         Assert.ThrowsException<DivideByZeroException>(() => Program.Divide("5", "0"));
     }
 
     	[TestMethod]
-    public void Invalid_Input()
+    public void Invalid_Input_Martinez()
     {
         Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "5"));
         Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "b"));
@@ -76,7 +76,7 @@ public class Addition
     }
 
     	[TestMethod]
-    public void Null_Input()
+    public void Null_Input_Martinez()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "5"));
         Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
